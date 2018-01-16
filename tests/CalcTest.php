454d8a7c2c0e61cf29918c125f8ac85837f8d397
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2018-01-16 11:22:06 +0900

    add test

---
+++ tests/CalcTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Sample\Calc;
+
+class CalcTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calc = new Calc();
+        $this->assertEquals($calc->add(1, 2), 3);
+    }
+}
+?>
